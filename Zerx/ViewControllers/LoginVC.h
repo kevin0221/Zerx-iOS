ebd982f683242ebb3e649718672b3b24b99d5559
kevin nissen <kevin.nissen0714@hotmail.com>
2016-06-16 23:35:49 +0200

    init commit

---
+++ Zerx/ViewControllers/LoginVC.h
@@ -0,0 +1,13 @@
+//
+//  LoginVC.h
+//  Zerx
+//
+//  Created by beauty on 6/16/16.
+//  Copyright © 2016 kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginVC : UIViewController
+
+@end
---
+++ ZerxTests/ZerxTests.m
@@ -0,0 +1,39 @@
+//
+//  ZerxTests.m
+//  ZerxTests
+//
+//  Created by beauty on 6/16/16.
+//  Copyright © 2016 kevin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZerxTests : XCTestCase
+
+@end
+
+@implementation ZerxTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
